97a90793c71f2eead22915d3a5603789bf92b49c
Aleksandr Timakov <alex-timakov@ya.ru>
2018-05-17 20:47:04 +0700

    Create project with ViewControllers

---
+++ Authorization/StartVC.swift
@@ -0,0 +1,24 @@
+//
+//  StartVC.swift
+//  Authorization
+//
+//  Created by Aleksandr Timakov on 17.05.2018.
+//  Copyright © 2018 Aleksandr Timakov. All rights reserved.
+//
+
+import UIKit
+
+class StartVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
