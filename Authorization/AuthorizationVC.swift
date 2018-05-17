442086cb514e1c780d408aada7f0e847d9bf3883
Aleksandr Timakov <alex-timakov@ya.ru>
2018-05-17 20:47:21 +0700

    Create project

---
+++ Authorization/AuthorizationVC.swift
@@ -0,0 +1,23 @@
+//
+//  AuthorizationVC.swift
+//  Authorization
+//
+//  Created by Aleksandr Timakov on 17.05.2018.
+//  Copyright © 2018 Aleksandr Timakov. All rights reserved.
+//
+
+import UIKit
+
+class AuthorizationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
