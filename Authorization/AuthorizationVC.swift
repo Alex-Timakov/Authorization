1380e9afac44b657896b7859efe9bb08f554ca32
Aleksandr Timakov <alex-timakov@ya.ru>
2018-05-18 09:44:29 +0700

    Add Show/Hide Keyboard

--- Authorization/AuthorizationVC.swift
+++ Authorization/AuthorizationVC.swift
@@ -11,10 +11,28 @@ import UIKit
 class AuthorizationVC: UIViewController {
    
     @IBOutlet weak var forgetPassButton: UIButton!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         forgetPassButton.layer.borderColor = #colorLiteral(red: 0.9215686275, green: 0.9215686275, blue: 0.9215686275, alpha: 1).cgColor
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+   @objc func keyboardWillShow(_ notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            bottomConstraint.constant = keyboardSize.height
+        }
+    }
+    @objc func keyboardWillHide(_ notification: Notification) {
+        bottomConstraint.constant = 0
     }
 
 }
